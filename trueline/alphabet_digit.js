c4b65ce5d19a4b53898bcbe58180cb6c2b57ed43
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-09-16 10:47:41 +0530

    16/9/24 3 task done

---
+++ trueline/alphabet_digit.js
@@ -0,0 +1,9 @@
+var char = "4";
+
+if ((char >= "a" && char <= "z") || (char >= "A" && char <= "Z")) {
+  console.log("The character is an alphabet");
+} else if (char >= "0" && char <= "9") {
+  console.log("The character is a digit");
+} else {
+  console.log("The character is a special character");
+}
