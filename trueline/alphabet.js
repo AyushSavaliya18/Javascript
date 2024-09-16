7bcc7548c8957570a02f75855b733564a8a3d9dc
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-09-16 10:48:15 +0530

    16/9/24 -3 task done

---
+++ trueline/alphabet.js
@@ -0,0 +1,7 @@
+var char = "a";
+
+if ((char >= "a" && char <= "z") || (char >= "A" && char <= "Z")) {
+  console.log("The character is an alphabet");
+} else {
+  console.log("The character is not an alphabet");
+}
---
+++ trueline/vovel_const.js
@@ -0,0 +1,18 @@
+let ch = "a";
+
+if (
+  ch == "a" ||
+  ch == "e" ||
+  ch == "i" ||
+  ch == "o" ||
+  ch == "u" ||
+  ch == "A" ||
+  ch == "E" ||
+  ch == "I" ||
+  ch == "O" ||
+  ch == "U"
+) {
+  console.log("The Enterd character is a Vovel");
+} else {
+  console.log("The Enterd character is a Constant");
+}
